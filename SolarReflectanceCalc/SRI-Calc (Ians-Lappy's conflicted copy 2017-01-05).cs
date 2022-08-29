da52f712023d8c21df0ca5fd3f9c1c1422c2941e
ispringer76 <ispringer76@users.noreply.github.com>
2022-08-29 16:35:31 -0500

    Add files via upload

---
+++ SolarReflectanceCalc/SRI-Calc (Ians-Lappy's conflicted copy 2017-01-05).cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Deployment.Application;
+using Microsoft.Win32;
+using System.IO;
+
+namespace WindowsFormsApplication1
+    {
+    public partial class SRI : Form
+        {
+        public SRI()
+            {
+            InitializeComponent();
+            if (!System.Diagnostics.Debugger.IsAttached)
+                {
+                SetAddRemoveProgramsIcon();
+                }
+            }
+
+        private void Calculate_Click(object sender, EventArgs e)
+            {
+            try
+                {
+                //Initialize my variables
+                double E, A, dblOut5, dblOut12, dblOut30, X5, X12, X30,Tb, Ts, Tw;
+
+                //Convert text to double
+                E = double.Parse(tbE.Text);
+                A = double.Parse(tbAlpha.Text);
+
+                //Reset color of text box to default in case it was changed to yellow earlier
+                tbAlpha.BackColor = default(Color);
+                tbE.BackColor = default(Color);
+                //Hide error message label if it was set to visible
+                lblError.Visible = false;
+
+                // Check all our inputs first
+                if ((string.IsNullOrEmpty(tbE.Text) == true) && (string.IsNullOrEmpty(tbAlpha.Text) == true))
+                    {
+                    if (string.IsNullOrEmpty(tbE.Text) == true)
+                        {
+                        tbE.BackColor = System.Drawing.Color.Yellow;
+                        lblError.Visible = true;
+                        }
+                    if (string.IsNullOrEmpty(tbAlpha.Text) == true)
+                        {
+                        tbAlpha.BackColor = System.Drawing.Color.Yellow;
+                        lblError.Visible = true;
+                        }
+                    }
+
+                //If tbE out of range then change it yellow and displays error message
+                if ((E > .999) || (E < .001))
+                    {
+                    tbE.BackColor = System.Drawing.Color.Yellow;
+                    lblError.Visible = true;
+                    }
+                //if tbAlpha out of range change it yellow and display error message
+                if ((A > .999) || (A < .001))
+                    {
+                    tbAlpha.BackColor = System.Drawing.Color.Yellow;
+                    lblError.Visible = true;
+                    }
+                //Validate inputs again.
+                //Dumb, but I'm too lazy to fix it to an else as of yet
+                //TODO: fix it!
+                if (((E < 1.000) && (E > .000)) && ((A < 1.000) && (A > .000)) && (string.IsNullOrEmpty(tbE.Text) != true) && (string.IsNullOrEmpty(tbAlpha.Text) != true))
+                    {
+
+                    Ts = (309.7 + ((1066.07*A) - (31.98*E)) / ((6.78*E)+5)) - ((890.94*A*A) + (2153.86*A*E))/((6.78*E+5)*(6.78*E+5));
+                    Tb = (309.7 + ((1066.07 * 0.95) - (31.98 * 0.9)) / ((6.78 * 0.9) + 5)) - ((890.94 * 0.95 * 0.95) + (2153.86 * 0.95 * 0.9)) / ((6.78 * 0.90 + 5) * (6.78 * 0.90 + 5));
+                    Tw = (309.7 + ((1066.07 * 0.20) - (31.98 * 0.9)) / ((6.78 * 0.9) + 5)) - ((890.94 * 0.20 * 0.20) + (2153.86 * 0.20 * 0.9)) / ((6.78 * 0.90 + 5) * (6.78 * 0.90 + 5));
+                    X5 = 100.0 * (Tb - Ts) / (Tb - Tw);
+                    dblOut5 = Convert.ToInt32(X5);
+                    tbOut5.Text = dblOut5.ToString();
+
+
+                    Ts = (309.7 + ((1066.07 * A) - (31.98 * E)) / ((6.78 * E) + 12)) - ((890.94 * A * A) + (2153.86 * A * E)) / ((6.78 * E + 12) * (6.78 * E + 12));
+                    Tb = (309.7 + ((1066.07 * 0.95) - (31.98 * 0.9)) / ((6.78 * 0.9) + 12)) - ((890.94 * 0.95 * 0.95) + (2153.86 * 0.95 * 0.9)) / ((6.78 * 0.90 + 12) * (6.78 * 0.90 + 12));
+                    Tw = (309.7 + ((1066.07 * 0.20) - (31.98 * 0.9)) / ((6.78 * 0.9) + 12)) - ((890.94 * 0.20 * 0.20) + (2153.86 * 0.20 * 0.9)) / ((6.78 * 0.90 + 12) * (6.78 * 0.90 + 12));
+                    X12 = 100.0 * (Tb - Ts) / (Tb - Tw);
+                    dblOut12 = Convert.ToInt32(X12);
+                    tbOut12.Text = dblOut12.ToString();
+
+                    Ts = (309.7 + ((1066.07 * A) - (31.98 * E)) / ((6.78 * E) + 30)) - ((890.94 * A * A) + (2153.86 * A * E)) / ((6.78 * E + 30) * (6.78 * E + 30));
+                    Tb = (309.7 + ((1066.07 * 0.95) - (31.98 * 0.9)) / ((6.78 * 0.9) + 30)) - ((890.94 * 0.95 * 0.95) + (2153.86 * 0.95 * 0.9)) / ((6.78 * 0.90 + 30) * (6.78 * 0.90 + 30));
+                    Tw = (309.7 + ((1066.07 * 0.20) - (31.98 * 0.9)) / ((6.78 * 0.9) + 30)) - ((890.94 * 0.20 * 0.20) + (2153.86 * 0.20 * 0.9)) / ((6.78 * 0.90 + 30) * (6.78 * 0.90 + 30));
+                    X30 = 100.0 * (Tb - Ts) / (Tb - Tw);
+                    dblOut30 = Convert.ToInt32(X30);
+                    tbOut30.Text = dblOut30.ToString();
+                    }
+
+                }
+
+            catch (Exception err)
+                {
+                System.Diagnostics.Debug.WriteLine("An error occurred: '{0}'", err.Message);
+                }
+            }
+
+        private static void SetAddRemoveProgramsIcon()
+            {
+            //Only execute on a first run after first install or after update
+            if (ApplicationDeployment.CurrentDeployment.IsFirstRun && ApplicationDeployment.IsNetworkDeployed)
+                {
+                try
+                    {
+                    // Set the name of the application EXE file - make sure to include `,0` at the end.
+                    // Does not work for ClickOnce applications as far as I could figure out... Note, this will probably work
+                    // when run from Visual Studio, but not when deployed.
+                    //string iconSourcePath = Path.Combine(System.Windows.Forms.Application.StartupPath, "example.exe,0");
+                    // Reverted to using this instead (note this will probably not work when run from Visual Studio, but will work on deploy.
+                    string iconSourcePath = Path.Combine(System.Windows.Forms.Application.StartupPath, "Icon 32x32.ico");
+                    //MessageBox.Show(iconSourcePath);
+                    if (!File.Exists(iconSourcePath))
+                        {
+                        System.Diagnostics.Debug.WriteLine("We could not find the application icon. Please notify your software vendor of this error.");
+                        return;
+                        }
+
+                    RegistryKey myUninstallKey = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Uninstall");
+                    string[] mySubKeyNames = myUninstallKey.GetSubKeyNames();
+                    for (int i = 0; i < mySubKeyNames.Length; i++)
+                        {
+                        RegistryKey myKey = myUninstallKey.OpenSubKey(mySubKeyNames[i], true);
+                        object myValue = myKey.GetValue("DisplayName");
+                        System.Diagnostics.Debug.WriteLine(myValue.ToString());
+                        //MessageBox.Show(myValue.ToString());
+                        // Set this to the display name of the application. If you are not sure, browse to the registry directory and check.
+                        if (myValue != null && myValue.ToString() == "Solar Reflectance Calculator")
+                            {
+                            MessageBox.Show("here");
+                            myKey.SetValue("DisplayIcon", iconSourcePath);
+                            break;
+                            }
+                        }
+                    }
+
+                catch (Exception err)
+                    {
+                    System.Diagnostics.Debug.WriteLine("We could not properly setup the application icons, an error occurred: '{0}'", err.Message);
+                    //MessageBox.Show("We could not properly setup the application icons. Please notify your software vendor of this error.\r\n" + mye.ToString());
+                    }
+                }
+            }
+        }
+    }
---
+++ SolarReflectanceCalc/SRI-Calc.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace SolarReflectanceCalc
+    {
+    partial class SRI
+        {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+            {
+            if (disposing && (components != null))
+                {
+                components.Dispose();
+                }
+            base.Dispose(disposing);
+            }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+            {
+            this.tbE = new System.Windows.Forms.TextBox();
+            this.tbAlpha = new System.Windows.Forms.TextBox();
+            this.Calculate = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbOut30 = new System.Windows.Forms.TextBox();
+            this.tbOut12 = new System.Windows.Forms.TextBox();
+            this.tbOut5 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblError = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbE
+            // 
+            this.tbE.AllowDrop = true;
+            this.tbE.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbE.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbE.Location = new System.Drawing.Point(106, 218);
+            this.tbE.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbE.MaxLength = 5;
+            this.tbE.Name = "tbE";
+            this.tbE.Size = new System.Drawing.Size(105, 50);
+            this.tbE.TabIndex = 0;
+            this.tbE.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tbAlpha
+            // 
+            this.tbAlpha.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbAlpha.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbAlpha.Location = new System.Drawing.Point(307, 218);
+            this.tbAlpha.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbAlpha.MaxLength = 5;
+            this.tbAlpha.Name = "tbAlpha";
+            this.tbAlpha.Size = new System.Drawing.Size(104, 50);
+            this.tbAlpha.TabIndex = 1;
+            this.tbAlpha.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Calculate
+            // 
+            this.Calculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Calculate.Location = new System.Drawing.Point(176, 364);
+            this.Calculate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Calculate.Name = "Calculate";
+            this.Calculate.Size = new System.Drawing.Size(160, 76);
+            this.Calculate.TabIndex = 2;
+            this.Calculate.Text = "Calculate";
+            this.Calculate.UseVisualStyleBackColor = true;
+            this.Calculate.Click += new System.EventHandler(this.Calculate_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.tbOut30);
+            this.groupBox1.Controls.Add(this.tbOut12);
+            this.groupBox1.Controls.Add(this.tbOut5);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(16, 471);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(512, 320);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(82, 60);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(344, 38);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "Convection Coefficient";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(370, 123);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 29);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "30";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(227, 123);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 29);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "12";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(92, 123);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 29);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "5";
+            // 
+            // tbOut30
+            // 
+            this.tbOut30.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbOut30.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbOut30.Location = new System.Drawing.Point(342, 174);
+            this.tbOut30.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbOut30.Name = "tbOut30";
+            this.tbOut30.ReadOnly = true;
+            this.tbOut30.Size = new System.Drawing.Size(87, 50);
+            this.tbOut30.TabIndex = 2;
+            this.tbOut30.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tbOut12
+            // 
+            this.tbOut12.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbOut12.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbOut12.Location = new System.Drawing.Point(202, 174);
+            this.tbOut12.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbOut12.Name = "tbOut12";
+            this.tbOut12.ReadOnly = true;
+            this.tbOut12.Size = new System.Drawing.Size(91, 50);
+            this.tbOut12.TabIndex = 1;
+            this.tbOut12.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tbOut5
+            // 
+            this.tbOut5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbOut5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbOut5.Location = new System.Drawing.Point(62, 174);
+            this.tbOut5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbOut5.Name = "tbOut5";
+            this.tbOut5.ReadOnly = true;
+            this.tbOut5.Size = new System.Drawing.Size(93, 50);
+            this.tbOut5.TabIndex = 0;
+            this.tbOut5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(136, 170);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 38);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ε";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(22, 36);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(486, 44);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "SRI 1000 Solar Reflectance";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(121, 102);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(294, 44);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Index Calculator";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(352, 170);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 38);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "α";
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblError.ForeColor = System.Drawing.Color.DarkRed;
+            this.lblError.Location = new System.Drawing.Point(66, 292);
+            this.lblError.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblError.Name = "lblError";
+            this.lblError.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lblError.Size = new System.Drawing.Size(403, 29);
+            this.lblError.TabIndex = 9;
+            this.lblError.Text = "Input must be between .001 and .999";
+            this.lblError.Visible = false;
+            // 
+            // SRI
+            // 
+            this.AcceptButton = this.Calculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(544, 808);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Calculate);
+            this.Controls.Add(this.tbAlpha);
+            this.Controls.Add(this.tbE);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "SRI";
+            this.Text = "Solar Reflective Index";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+            }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbE;
+        private System.Windows.Forms.TextBox tbAlpha;
+        private System.Windows.Forms.Button Calculate;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbOut30;
+        private System.Windows.Forms.TextBox tbOut12;
+        private System.Windows.Forms.TextBox tbOut5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblError;
+        }
+    }
+
